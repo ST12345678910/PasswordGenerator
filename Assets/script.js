3e2ebaf154949d0e02a33c9be458226734aa5d8c
Shaun <34657716+ST12345678910@users.noreply.github.com>
2022-02-18 13:15:34 -0800

    Added notes to script.js doc:
    - notes explain what each section does
    - changed standard function on lines 26 and 139 to arrow function
    - corrected wording if the user selects nothing, now says "You must choose atleast one criteria"

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+// variables that will be referenced, the user options that are available
 let charLength;
 let verNum;
 let verChar;
@@ -5,26 +6,31 @@ let verUpper;
 let verLower;
 let userOptions;
 
+// The arrays from which the password will be created
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 character = ["'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%", "&",];
 
+// placeholder for letters that are turned uppercase
 holderUpper = [];
 
+// converts lowercase to uppercase if option is selected by user
 let toUpper = (x) => x.toUpperCase();
 
 alphaUpper = alpha.map(toUpper);
 
 let get = document.querySelector("#generate");
 
-get.addEventListener("click", function () {
-  pg = passwordGen();
-  document.getElementById("password").placeholder = pg;
-});
+// starts the process when the #generate button is clicked
+get.addEventListener("click", () => {
+    pg = passwordGen();
+    document.getElementById("password").placeholder = pg;
+  });
 
 function passwordGen() {
 
+  //if the user does not chose an option for length, they will be told the input is invalid, if they chose a number out of the given range they will be told it is out of range
   charLength = parseInt(prompt("Choose password length, between 8 and 128 characters"));
 
   if (!charLength) {
@@ -34,7 +40,7 @@ function passwordGen() {
     charLength = parseInt(prompt("Invalid Input: length must be between 8 - 128 characters"));
 
   } else {
-
+    // the options for criteria that are given to the user
     verNum = confirm("Contain numbers?");
     verChar = confirm("Contain special characters?");
     verUpper = confirm("Contain uppercase characters?");
@@ -42,51 +48,67 @@ function passwordGen() {
   };
 
   switch (userOptions = true) {
+    // prompts the user to pick atleast one option, if they have picked none
     case userOptions = (!verChar && !verNum && !verUpper && !verLower):
-      userOptions = alert("You must choose within the range")
+      userOptions = alert("You must choose atleast one criteria")
       break;
+      // case for all options chosen
     case userOptions = verChar && verNum && verUpper && verLower:
       userOptions = character.concat(number, alpha, alphaUpper);
       break;
+      // case for special characters, numbers, and uppercase letters
     case userOptions = verChar && verNum && verUpper: 
       userOptions = character.concat(number, alphaUpper);
       break;
+      // case for special characters, numbers, lowercase letters
     case userOptions = verChar && verNum && verLower:
       userOptions = character.concat(number, alpha);
       break;
+      // case for special characters, lowercase and uppercase
     case userOptions = verChar && verLower && verUpper:
       userOptions = character.concat(alpha, alphaUpper);
       break;
+      // case for numbers, lowercase and uppercase
     case userOptions = verNum && verLower && verUpper:
       userOptions = number.concat(alpha, alphaUpper);
       break;
+      // case for special characters and numbers
     case userOptions = verChar && verNum:
       userOptions = character.concat(number);
       break;
+      // case for special characters and lowercase letters
     case userOptions = verChar && verLower:
       userOptions = character.concat(alpha);
       break;
+      // case for special characters and uppercase letters
     case userOptions = verChar && verUpper:
       userOptions = character.concat(alphaUpper);
       break;
+      // case for lowercase letters and numbers
     case userOptions = verLower && verNum:
       userOptions = alpha.concat(number);
       break;
+      //case for lowercase and uppercase letters
     case userOptions = verLower && verUpper:
       userOptions = alpha.concat(alphaUpper);
       break;
+      // case for numbers and uppercase letters
     case userOptions = verNum && verUpper:
       userOptions = number.concat(alphaUpper);
       break;
+      // case for only special characters
     case userOptions = verChar:
       userOptions = character;
       break;
+      // case for only numbers
     case userOptions = verNum:
       userOptions = number;
       break;
+      // case for only lowercase letters
     case userOptions = verLower:
       userOptions = alpha;
       break;
+      //case for only uppercase letters
     case userOptions = verUpper:
       userOptions = holderUpper.concat(alphaUpper);
       break;
@@ -94,27 +116,31 @@ function passwordGen() {
   
   let password = [];
 
-
+// picks randomly from all available selection
   for (let i = 0; i < charLength; i++) {
     let pickuserOptions = userOptions[Math.floor(Math.random() * userOptions.length)];
     password.push(pickuserOptions);
   }
 
+  // forms into string (.join concats everyting in the array)
   let pg = password.join("");
   userInput(pg);
   return pg;
 }
 
+// puts the password into viewable text for the user
 function userInput(pg) {
   document.getElementById("password").textContent = pg;
 
 }
 
+// lets the user press the button to copy the password
 let copy = document.querySelector("#select");
-copy.addEventListener("click", function () {
-  passwordSelect();
-});
+copy.addEventListener("click", () => {
+    passwordSelect();
+  });
 
+  // tells the user that their password has been highlighted
 function passwordSelect() {
   document.getElementById("password").select();
   document.execCommand("select");
